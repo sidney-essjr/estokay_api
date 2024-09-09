faef03a597e57e44284f5eaeb1f3aea0d82c0b7f
Sidney Espirito Santo de Souza Junior <sidney.souza@compasso.com.br>
2024-09-09 14:05:56 -0300

    initial commit

---
+++ src/test/java/com/estokay/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estokay.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
