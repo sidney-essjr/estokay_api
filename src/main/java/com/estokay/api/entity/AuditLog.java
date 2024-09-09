4125cfbeaea9d71eb1d19f278926ed289a0c6aa3
Sidney Espirito Santo de Souza Junior <sidney.souza@compasso.com.br>
2024-09-09 17:01:45 -0300

    feat: added the AuditLog entity

---
+++ src/main/java/com/estokay/api/entity/AuditLog.java
@@ -0,0 +1,54 @@
+package com.estokay.api.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "audit_logs")
+@Getter @Setter @ToString
+@AllArgsConstructor @NoArgsConstructor
+public class AuditLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "record_id")
+    private long recordId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "Operation", nullable = false, length = 20)
+    private Operation operationType = Operation.UNCHANGED;
+
+    @Column(name = "created_in", nullable = false)
+    private LocalDateTime createdIn;
+
+    @Column(name = "changed_at", nullable = false)
+    private LocalDateTime changedAt;
+
+    @Column(name = "changed_by", nullable = false)
+    private String changedBy;
+
+    @Column(name = "old_value")
+    private String oldValue;
+
+    public enum Operation {
+        INSERT, UPDATE, DELETE, UNCHANGED
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AuditLog auditLog = (AuditLog) o;
+        return id == auditLog.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
