c3fcfcf01ea7ccf6c7f0a037f8ef1563f7edc6fc
Sidney Espirito Santo de Souza Junior <sidney.souza@compasso.com.br>
2024-09-09 17:24:48 -0300

    feat: added DonorService class

--- src/main/java/com/estokay/api/service/DonorService.java
+++ src/main/java/com/estokay/api/service/DonorService.java
@@ -1,4 +1,25 @@
 package com.estokay.api.service;
 
+import com.estokay.api.entity.Donor;
+import com.estokay.api.exception.EmailUniqueViolationException;
+import com.estokay.api.repository.DonorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class DonorService {
+
+    private DonorRepository donorRepository;
+
+    @Transactional
+    public Donor save(Donor donor) {
+        try {
+            return donorRepository.save(donor);
+        } catch (org.springframework.dao.DataIntegrityViolationException ex) {
+            throw new EmailUniqueViolationException(String.format("The donor's email '%s' is already in use", donor.getEmail()));
+        }
+    }
+
 }
