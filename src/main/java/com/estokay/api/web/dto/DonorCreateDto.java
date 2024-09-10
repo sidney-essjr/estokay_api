bc512efbab85f45b064dd1cbdde829fe71f579c1
Sidney Espirito Santo de Souza Junior <sidney.souza@compasso.com.br>
2024-09-10 11:11:13 -0300

    feat: added DonorCreateDto

--- src/main/java/com/estokay/api/web/dto/DonorCreateDto.java
+++ src/main/java/com/estokay/api/web/dto/DonorCreateDto.java
@@ -1,4 +1,35 @@
-package com.estokay.api.web.dto.mapper;
+package com.estokay.api.web.dto;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@ToString
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class DonorCreateDto {
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    @Size(min = 10)
+    private String phone;
+
+    @NotBlank
+    @Email(regexp = "^[a-z0-9.+-]+@[a-z0-9.-]+\\.[a-z]{2,}$")
+    private String email;
+
+    private String code;
+
+    private String address;
+
+    @NotBlank
+    private String city;
+
+    @NotBlank
+    private String state;
 }
--- src/main/java/com/estokay/api/web/dto/mapper/DonorMapper.java
+++ src/main/java/com/estokay/api/web/dto/mapper/DonorMapper.java
@@ -1,4 +1,12 @@
 package com.estokay.api.web.dto.mapper;
 
+import com.estokay.api.entity.Donor;
+import com.estokay.api.web.dto.DonorCreateDto;
+import org.modelmapper.ModelMapper;
+
 public class DonorMapper {
+
+    public static Donor toDonor(DonorCreateDto dto) {
+        return new ModelMapper().map(dto, Donor.class);
+    }
 }
