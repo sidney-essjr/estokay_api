b5008515b98dc3f1f6b95edbb20fb3126600b50d
sidney-essjr <sidney@sessjr.com>
2024-09-10 16:14:43 -0300

    feat: added findAll and update services

--- src/main/java/com/estokay/api/service/DonorService.java
+++ src/main/java/com/estokay/api/service/DonorService.java
@@ -4,9 +4,16 @@ import com.estokay.api.entity.Donor;
 import com.estokay.api.exception.EmailUniqueViolationException;
 import com.estokay.api.exception.EntityNotFoundException;
 import com.estokay.api.repository.DonorRepository;
+import com.estokay.api.web.dto.DonorCreateDto;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -23,9 +30,34 @@ public class DonorService {
         }
     }
 
-    public Donor getById(long id) {
+    @Transactional(readOnly = true)
+    public Donor findById(long id) {
         return donorRepository.findById(id).orElseThrow(
                 () -> new EntityNotFoundException(String.format("The id entity %d was not found", id))
         );
+    }
+
+    @Transactional(readOnly = true)
+    public List<Donor> findAll() {
+        return donorRepository.findAll();
+    }
+
+    @Transactional
+    public void update(long id, DonorCreateDto dto) {
+        Donor donor = findById(id);
+        BeanUtils.copyProperties(dto, donor, getNullPropertyNames(dto));
+    }
+
+    private String[] getNullPropertyNames(Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
+        java.util.List<String> nullPropertyNames = new java.util.ArrayList<>();
+        for (java.beans.PropertyDescriptor pd : pds) {
+            Object srcValue = src.getPropertyValue(pd.getName());
+            if (srcValue == null) {
+                nullPropertyNames.add(pd.getName());
+            }
+        }
+        return nullPropertyNames.toArray(new String[0]);
     }
 }
