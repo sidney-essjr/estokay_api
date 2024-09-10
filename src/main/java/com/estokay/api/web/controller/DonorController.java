d9fba9585eb91119f8bd60df582e794cff24b67f
sidney-essjr <sidney@sessjr.com>
2024-09-10 12:33:28 -0300

    feat: added DonorController

---
+++ src/main/java/com/estokay/api/web/controller/DonorController.java
@@ -0,0 +1,28 @@
+package com.estokay.api.web.controller;
+
+import com.estokay.api.entity.Donor;
+import com.estokay.api.service.DonorService;
+import com.estokay.api.web.dto.DonorCreateDto;
+import com.estokay.api.web.dto.mapper.DonorMapper;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/donors")
+public class DonorController {
+
+    private final DonorService donorService;
+
+    @PostMapping
+    public ResponseEntity<Donor> save(@Valid @RequestBody DonorCreateDto donorDto) {
+        Donor donor = donorService.save(DonorMapper.toDonor(donorDto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(donor);
+    }
+}
