bc16674878b3a573f84b14428089bc1f1ebe6e7d
sidney-essjr <sidney@sessjr.com>
2024-09-10 16:16:15 -0300

    feat: added mapper method toListDto

--- src/main/java/com/estokay/api/web/dto/mapper/DonorMapper.java
+++ src/main/java/com/estokay/api/web/dto/mapper/DonorMapper.java
@@ -5,6 +5,9 @@ import com.estokay.api.web.dto.DonorCreateDto;
 import com.estokay.api.web.dto.DonorResponseDto;
 import org.modelmapper.ModelMapper;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class DonorMapper {
 
     public static Donor toDonor(DonorCreateDto dto) {
@@ -14,4 +17,9 @@ public class DonorMapper {
     public static DonorResponseDto toDto(Donor donor) {
         return new ModelMapper().map(donor, DonorResponseDto.class);
     }
+
+    public static List<DonorResponseDto> toListDto(List<Donor> donors) {
+        return donors.stream().map(DonorMapper::toDto).collect(Collectors.toList());
+    }
+
 }
