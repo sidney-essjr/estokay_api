0d37bab32640fbfbd827a73a89595a64dcab55c0
sidney-essjr <sidney@sessjr.com>
2024-09-12 11:05:28 -0300

    feat: added ItemType entity

--- src/main/java/com/estokay/api/entity/ItemType.java
+++ src/main/java/com/estokay/api/entity/ItemType.java
@@ -1,4 +1,24 @@
 package com.estokay.api.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter @ToString
+@Table(name = "Item_Types")
 public class ItemType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
 }
