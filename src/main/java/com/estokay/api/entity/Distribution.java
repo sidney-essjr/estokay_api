ff31c9888c3c84ecd23bdadd960210007ccd9b7c
sidney-essjr <sidney@sessjr.com>
2024-09-13 13:34:05 -0300

    feat: added Distribution entity

--- src/main/java/com/estokay/api/entity/Distribution.java
+++ src/main/java/com/estokay/api/entity/Distribution.java
@@ -1,4 +1,36 @@
 package com.estokay.api.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter @ToString
+@Table(name = "Distribution")
 public class Distribution {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "beneficiary_id", nullable = false)
+    private Beneficiary beneficiary;
+
+    @OneToMany
+    @JoinColumn(name = "distribution_id", nullable = false)
+    private List<Donation> donations;
+
+    @Column(name = "distribution_date", nullable = false)
+    private LocalDateTime distributionDate;
+
+    @ManyToOne
+    @JoinColumn(name = "audit_id", nullable = false)
+    private AuditLog audit;
+
 }
